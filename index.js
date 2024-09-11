8dffe328e9d437538327b52484f83fa4030f53c0
Moshe Goldstein <mygguy613@gmail.com>
2024-09-11 17:38:47 -0400

    cl

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(emp = '*') {
+  return function (adj = 'special') {
+    console.log(`You are ${emp}${adj}${emp}!`);
+    return `You are ${emp}${adj}${emp}!`
+  }
+}
+
+wrapAdjective('!!')('a dedicated programmer');
